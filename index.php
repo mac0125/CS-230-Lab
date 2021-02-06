7e0196e7867003d58a5b18ff054f5ff6982f45ea
mac0125 <mac0125@mix.wvu.edu>
2021-02-05 20:36:47 -0500

    About Page (Lab 2)

---
+++ index.php
@@ -0,0 +1,55 @@
+<body>
+    <link rel="stylesheet" href="css/about.css">
+
+    <div class="about">
+        <h1>About Us</h1>
+        <p>Explain in a paragraph</p>
+    </div>
+
+    <div class="row">
+        <div class="column">
+            <div class="card">
+                <img src="images/TonyStark.jpeg">
+                <div class="container">
+                    <h2>Founder</h2>
+                    <p>Anthony Stark</p>
+                    <p>Email: thestark1@starkindustries.com</p>
+                </div>
+            </div>
+        </div>
+
+        <div class="column">
+            <div class="card">
+                <img src="images/Shuri.jpeg">
+                <div class="container">
+                    <h2>Head Editor</h2>
+                    <p>Shuri</p>
+                    <p>Email: shurilab@starkindustries.com</p>
+                </div>
+            </div>
+        </div>
+
+        <div class="column">
+            <div class="card">
+                <img src="images/PeterParker.jpeg">
+                <div class="container">
+                    <h2>Head Reviewer</h2>
+                    <p>Peter Parker</p>
+                    <p>Email: queens2000@starkindustries.com</p>
+                </div>
+            </div>
+        </div>
+
+        <div class="column">
+            <div class="card">
+                <img src="images/DaisyJohnson.jpeg">
+                <div class="container">
+                    <h2>Website & Media Manager</h2>
+                    <p>Daisy Johnson</p>
+                    <p>Email: daisyj1988@starkindustries.com</p>
+                </div>
+            </div>
+        </div>
+
+    </div>
+</body>
