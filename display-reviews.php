d6a3555451850594aeadbca1cbfd326573e73447
mac0125 <70492401+mac0125@users.noreply.github.com>
2021-03-27 22:31:53 -0400

    Update display-reviews.php

--- display-reviews.php
+++ display-reviews.php
@@ -2,7 +2,7 @@
 
 $servename = "localhost";
 $DBuname = "phpmyadmin";
-$DBPass = "Trey110211";
+$DBPass = "cs230lab";
 $DBname = "cs230";
 
 $conn = mysqli_connect($servename, $DBuname, $DBPass, $DBname);
